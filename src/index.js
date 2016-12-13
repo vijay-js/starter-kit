c5f5d500384ff26f935a88cfdb2402e5cf3118e0
VGOD <vijay4git@gmail.com>
2016-12-13 18:45:06 +0530

    ESLINT Definitions

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import numeral from 'numeral';
 import './index.css';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this course`);
+console.log(`I would pay ${courseValue} for this course`); //eslint-disable-line no-console
