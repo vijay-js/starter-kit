61449e04b1ad4172dbabe054881bcb273348167c
VGOD <vijay4git@gmail.com>
2016-12-21 13:13:51 +0530

    Webpack Setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ import path from 'path';
 export default {
   debug: true,
   devtool: 'inline-source-map',
-  noInfo: false,
+  noInfo: false, //display the list of all files its bundling . Set this noise off in prod build
   entry: [
     path.resolve(__dirname, 'src/index')
   ],
@@ -16,7 +16,7 @@ export default {
   plugins: [],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']}, // loaders teach webpack on how to handle different file types
       {test: /\.css$/, loaders: ['style','css']}
     ]
   }
